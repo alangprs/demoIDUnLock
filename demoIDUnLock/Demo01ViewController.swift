afdbc79cb0788e5eb60e02ecb4b811f2b9685c87
will <will.weng@lig.com.tw>
2021-12-22 14:54:38 +0800

    完成生物識別登入功能

---
+++ demoIDUnLock/Demo01ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Demo01ViewController.swift
+//  demoIDUnLock
+//
+//  Created by will on 2021/12/22.
+//
+
+import UIKit
+
+class Demo01ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    
+
+}
--- demoIDUnLock/ViewController.swift
+++ demoIDUnLock/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import LocalAuthentication
+
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func idUnlock(_ sender: UIButton) {
+        
+        let context = LAContext()
+        // 宣告一個變數接收 canEvaluatePolicy 返回的錯誤
+        var error: NSError?
+        // 設置取消按鈕標題
+        context.localizedCancelTitle = "取消"
+        // 評估是否可以針對給定方案進行身份驗證
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+            // 描述使用身份辨識的原因
+            let reason = "使用生物辨識功能登入"
+            // 指定登入方案
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { (success, error) in
+                if success {
+                    DispatchQueue.main.async { [weak self] in
+                        //成功時，執行alert內容
+                        self?.showMessage(title: "登入成功", message: nil)
+                    }
+                } else {
+                    DispatchQueue.main.async { [weak self] in
+                        //失敗時，執行alert內容
+                        self?.showMessage(title: "登入失敗", message: error?.localizedDescription)
+                    }
+                }
+            }
+        } else {
+            showMessage(title: "登入失敗", message: error?.localizedDescription)
+        }
+        
+    }
+ 
+    func showMessage(title: String?, message: String?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "確定", style: .cancel)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
